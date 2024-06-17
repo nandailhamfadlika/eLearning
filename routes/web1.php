b304fe18fc393ccc6221c740508de675e2f51ab0
nandailhamfadlika <nandailhamfadlika@gmail.com>
2024-06-17 21:47:55 +0700

    Update Pertemuan 13 laravel auth

--- routes/web1.php
+++ routes/web1.php
@@ -1,31 +1,22 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CoursesController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\StudentController;
-
+use Illuminate\Support\Facades\Route;
 use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
+// Root Route
+Route::get('/', function () {
+    return view('welcome');
+});
 
 // Route::get('/salam',function(){
 //     return "Assalamualaikum";
 // });
 
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    // Dashboard Route
-    Route::get('admin/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-
+// Dashboard Route
+Route::get('admin/dashboard', [DashboardController::class, 'index']);
 Route::get('admin/student', [StudentController::class, 'index']);
 Route::get('admin/course', [CoursesController::class, 'index']);
 
@@ -59,12 +50,3 @@ Route::put('/admin/course/update/{id}', [CoursesController::class, 'update']);
 
 // route delete course
 Route::delete('/admin/course/delete/{id}', [CoursesController::class, 'destroy']);
-
-
-
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
